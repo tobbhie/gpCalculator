b3d6e723d60a4df6ccc687b8ada232c8e02cbd17
tobbhie <tobifolajin@gmail.com>
2019-03-23 06:16:37 +0400

    Final

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/*
+Author - Folajin Tobi
+Github Username - @tobbhie
+Instagram - @to_bbhie
+*/
+
+//Declaring all variables
+gpForm = document.querySelector('#gp-form');
+calculate = document.querySelector('.delete-course');
+addCourse = document.querySelector('.add-course');
+gradeTitle = document.querySelector('#grade');
+gradeContent = document.querySelector('#grade-row');
